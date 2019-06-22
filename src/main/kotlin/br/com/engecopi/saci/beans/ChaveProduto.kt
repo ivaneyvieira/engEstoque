96c3e0a54e565bd0ee2c56af638a957e2b79d850
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-06-21 23:59:15 -0300

    Ajustes no codigo de barras

--- src/main/kotlin/br/com/engecopi/saci/beans/ChaveProduto.kt
+++ src/main/kotlin/br/com/engecopi/saci/beans/ChaveProduto.kt
@@ -8,8 +8,8 @@ class ChaveProduto(val prdno: String, val grade: String, val barcode: String, va
 }
 
 fun List<ChaveProduto>.findChave(): ChaveProduto? {
-  val listaPdv = this.filter {it.tipo == "PDV" && it.grade != ""}
+  val listaPrd2 = this.filter {it.tipo == "PDV2" && it.grade != ""}
   val listaGrade = this.filter {it.tipo == "GRADE" && it.grade != ""}
   val listaPrd = this.filter {it.tipo == "PRD" && it.grade == ""}
-  return listaGrade.firstOrNull() /*?: listaPdv.firstOrNull() */ ?: listaPrd.firstOrNull()
+  return listaGrade.firstOrNull() ?: listaPrd2.firstOrNull()  ?: listaPrd.firstOrNull()
 }
--- src/main/resources/sqlSaci/findBarcode2.sql
+++ src/main/resources/sqlSaci/findBarcode2.sql
@@ -3,14 +3,23 @@ from sqlpdv.prdstk
 where storeno = :storeno
   and prdno = :prdno
   and grade = :grade
+  and barcode <> ''
 union
 select distinct prdno, grade, barcode, 'GRADE' as tipo
 from sqldados.prdbar
 where prdno =  :prdno
   and grade =  :grade
   and :grade != ''
+  and barcode <> ''
 union
 select distinct no as prdno, '' as grade, barcode, 'PRD' as tipo
 from sqldados.prd
-where no    = :prdno
+where no     = :prdno
   and :grade = ''
+  and barcode <> ''
+union
+select distinct prdno, '' as grade, auxString1 as barcode, 'PRD2' as tipo
+from sqldados.prd2
+where prdno  = :prdno
+  and :grade = ''
+  and auxString1 <> ''
