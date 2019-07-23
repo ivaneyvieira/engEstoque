513f88f738c1bd91713f1ab1c157932e1adcaeef
ivaneyvieira <ivaneyvieira@gmail.com>
2019-07-23 15:15:39 -0300

    deploy 5.74:

--- src/main/kotlin/br/com/engecopi/estoque/ui/LoginService.kt
+++ src/main/kotlin/br/com/engecopi/estoque/ui/LoginService.kt
@@ -34,15 +34,13 @@ object LoginService {
   fun login(loginInfo: LoginInfo) {
     EstoqueUI.current?.loginInfo = loginInfo
     Session[LoginInfo::class] = loginInfo
-//    val httpSession = Session.current.session as? HttpSession
-//    val ctx = Session.current.getContext() as WebApplicationContext
-    val session = ctx.getHttpSession()
-    VaadinSession.getAllSessions(httpSession ).forEach {
-      println("#####Session $it")
-    }
-    //Page.getCurrent().reload()
+    //    val httpSession = Session.current.session as? HttpSession
+    //    val ctx = Session.current.getContext() as WebApplicationContext
+    //    val session = ctx.getHttpSession()
+    //    VaadinSession.getAllSessions(httpSession ).forEach {
+    //    println("#####Session $it")
   }
-
+  //Page.getCurrent().reload()
   val currentUser: LoginInfo?
     get() = EstoqueUI.current?.loginInfo
 
