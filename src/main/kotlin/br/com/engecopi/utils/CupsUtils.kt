83c8f413419713172ebbc9103e1d9adf7432fc88
ivaneyvieira <ivaneyvieira@gmail.com>
2019-07-31 11:25:04 -0300

    Cores no grid de saída e preparação das impressões aplicadas ao deploy 5.74

--- src/main/kotlin/br/com/engecopi/utils/CupsUtils.kt
+++ src/main/kotlin/br/com/engecopi/utils/CupsUtils.kt
@@ -1,27 +1,32 @@
 package br.com.engecopi.utils
 
-import br.com.engecopi.framework.printer.PrinterCups
-import br.com.engecopi.framework.printer.print
+
+import org.cups4j.CupsClient
+import org.cups4j.CupsPrinter
 import org.cups4j.PrintJob
 
-fun main() {
-  val cupsClient = PrinterCups("192.168.1.14")
-  println("Inicio")
-  cupsClient.printers.forEach {printer ->
-    println("impressora")
-    printer.print(etiqueta)
-    println(printer.name)
-  }
-  val printer = cupsClient.printers.firstOrNull()
 
-  printer?.let {
-    println("imprimindo job")
-    val job = PrintJob.Builder(etiqueta.toByteArray())
+object CupsUtils {
+  private val cupsClient = CupsClient()
+  private val printers
+    get() = cupsClient.printers.toList()
+
+  fun printerExists(printerName: String) = printers.any {it.name == printerName}
+
+  fun printer(printerName: String) = printers.firstOrNull {it.name == printerName}
+
+  fun CupsPrinter.printText(text: String, resultMsg: (String) -> Unit = {}) {
+    val job = PrintJob.Builder(text.toByteArray())
       .build()
-    printer.print(job)
+    val result = print(job)
+    resultMsg("Job ${result.jobId}: ${result.resultDescription} : ${result.resultMessage}")
   }
 
-  println("Fim")
+  fun CupsPrinter.printerTeste() {
+    printText(etiqueta)
+  }
+
+  private const val etiqueta = "^XA\n" + "^FT20,070^A0N,70,50^FH^FDNF ENTRADA:1212^FS\n" + "^FT600,070^A0N,70," +
+                               "50^FH^FD30/06/18^FS\n" + "^FT20,140^A0N,70,50^FH^FDPRODUTO:000019^FS\n" + "^FT400,140^A0N,70,50^FH^FD - ^FS\n" + "^FT20,210^A0N,70,50^FH^FDTGR  SD ADA CT  20X012^FS\n" + "^FT20,280^A0N,70,50^FH^FDPALLET COM: 5CXS^FS\n" + "^FT20,350^A0N,70,50^FH^FDENTRADA: 1/5 PALLET^FS\n" + "^FT20,420^A0N,70,50^FH^FDESTOQUE: 1/5PALLET^FS\n" + "^FT220,650^A0N,250,300^FH^FD1^FS\n" + "^FO220,700^BY1^BCN,50,Y,N,N^FD000019  5 1/5^FS\n" + "^XZ"
 }
 
-const val etiqueta = "^XA\n" + "^FT20,070^A0N,70,50^FH^FDNF ENTRADA:1212^FS\n" + "^FT600,070^A0N,70,50^FH^FD30/06/18^FS\n" + "^FT20,140^A0N,70,50^FH^FDPRODUTO:000019^FS\n" + "^FT400,140^A0N,70,50^FH^FD - ^FS\n" + "^FT20,210^A0N,70,50^FH^FDTGR  SD ADA CT  20X012^FS\n" + "^FT20,280^A0N,70,50^FH^FDPALLET COM: 5CXS^FS\n" + "^FT20,350^A0N,70,50^FH^FDENTRADA: 1/5 PALLET^FS\n" + "^FT20,420^A0N,70,50^FH^FDESTOQUE: 1/5PALLET^FS\n" + "^FT220,650^A0N,250,300^FH^FD1^FS\n" + "^FO220,700^BY1^BCN,50,Y,N,N^FD000019  5 1/5^FS\n" + "^XZ"
