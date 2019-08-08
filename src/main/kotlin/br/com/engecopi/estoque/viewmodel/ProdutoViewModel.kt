b063b759b49778501ffb1bafddfacd7cf5eb8c9e
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-08-08 09:35:55 -0300

    deploy 5.83:

--- src/main/kotlin/br/com/engecopi/estoque/viewmodel/ProdutoViewModel.kt
+++ src/main/kotlin/br/com/engecopi/estoque/viewmodel/ProdutoViewModel.kt
@@ -5,6 +5,7 @@ import br.com.engecopi.estoque.model.LocProduto
 import br.com.engecopi.estoque.model.Loja
 import br.com.engecopi.estoque.model.Produto
 import br.com.engecopi.estoque.model.RegistryUserInfo.abreviacaoDefault
+import br.com.engecopi.estoque.model.RegistryUserInfo.userDefaultIsAdmin
 import br.com.engecopi.estoque.model.RegistryUserInfo.lojaDefault
 import br.com.engecopi.estoque.model.RegistryUserInfo.usuarioDefault
 import br.com.engecopi.estoque.model.Repositories
@@ -98,7 +99,7 @@ class ProdutoViewModel(view: IView): CrudViewModel<Produto, QProduto, ProdutoVo>
   fun localizacoes(bean: ProdutoVo?): List<LocProduto> {
     return bean?.produto?.localizacoes()
       .orEmpty()
-      .filter {it.startsWith(abreviacaoDefault)}
+      .filter {it.startsWith(abreviacaoDefault) || userDefaultIsAdmin}
       .map {LocProduto(it)}
   }
 
