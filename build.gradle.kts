1dfbffd6c1771790727211af09d19ca3c795a24f
ivaneyvieira <ivaneyvieira@gmail.com>
2019-09-09 12:28:03 -0300

    deploy 6.19: Correção do problema com o driver JDBC

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ dependencies {
   compile("com.vaadin:vaadin-client-compiled:$vaadin8Version")
   compile("javax.servlet:javax.servlet-api:3.1.0")
 
-  compile("mysql:mysql-connector-java:8.0.17")
+  compile("mysql:mysql-connector-java:5.1.48")
 
   compile("org.apache.commons:commons-dbcp2:2.3.0")
 
