7e4416fc3cb597ca407b1f423115a53fa53b3cf9
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-10-17 15:21:43 -0300

    deploy 6.46: Painel de transferencia de pedidos

--- src/main/resources/sqlSaci/findPedidoTransferencia.sql
+++ src/main/resources/sqlSaci/findPedidoTransferencia.sql
@@ -9,19 +9,20 @@ SELECT DISTINCT
        P.qtty / 1000 AS quant,
        C.name        AS clienteName,
        'PEDIDO_S'    AS tipo
-FROM sqldados.eord                      AS N
-         INNER JOIN sqldados.eoprd      AS P
+FROM sqldados.eord                  AS N
+         INNER JOIN sqldados.prdloc AS L
+                    USING (prdno, grade)
+         INNER JOIN sqldados.eoprd  AS P
                     USING (storeno, ordno)
-         INNER JOIN engEstoque.produtos AS E
-                    ON E.codigo = P.prdno AND E.grade = P.grade
-         INNER JOIN sqldados.custp      AS C
+         INNER JOIN sqldados.custp  AS C
                     ON C.no = N.custno
-         INNER JOIN sqldados.store      AS S
+         INNER JOIN sqldados.store  AS S
                     ON C.cpf_cgc = S.cgc
-         INNER JOIN sqldados.xroute     AS R
+         INNER JOIN sqldados.xroute AS R
                     ON R.storenoFrom = N.storeno AND R.storenoTo = S.no
 WHERE N.storeno = :storeno AND
-      N.status = 2
+      N.status = 2 AND
+      localizacao <> ''
 ORDER BY N.date DESC
 
 
