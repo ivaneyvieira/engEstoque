796c14fd6b852325bac113afe71962585b15c622
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2019-12-01 08:32:52 -0300

    deploy 6.67:

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ plugins {
   id("org.gretty") version "2.3.1"
   id("com.devsoap.plugin.vaadin") version "2.0.0.beta2"
   id("io.ebean") version "12.1.5"
+  war
 }
 
 repositories {
@@ -35,9 +36,6 @@ defaultTasks("clean", "build")
 group = "engEstoque"
 version = "1.0-SNAPSHOT"
 
-apply(plugin = "war")
-apply(plugin = "kotlin")
-apply(plugin = "io.ebean")
 
 gretty {
   contextPath = "/"
@@ -68,57 +66,63 @@ tasks.named<Test>("test") {
 
 dependencies {
   // Karibu-DSL dependency
-  implementation("com.github.mvysny.karibudsl:karibu-dsl-v8:$karibuVersion")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  api("com.github.mvysny.karibudsl:karibu-dsl-v8:$karibuVersion")
+  api("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  api("org.jetbrains.kotlin:kotlin-reflect")
   
-  implementation("ch.qos.logback:logback-classic:1.2.3")
-  implementation("org.slf4j:slf4j-api:1.7.25")
+  api("ch.qos.logback:logback-classic:1.2.3")
+  api("org.slf4j:slf4j-api:1.7.25")
   
-  implementation("org.slf4j:jul-to-slf4j:1.7.25")
+  api("org.slf4j:jul-to-slf4j:1.7.25")
   
   testImplementation("com.github.mvysny.dynatest:dynatest:0.8")
   
-  implementation("io.ebean:ebean:12.1.3")
-  implementation("io.ebean:ebean-querybean:12.1.3")
-  implementation("io.ebean:ebean-agent:12.1.3")
-  implementation("io.ebean.tools:finder-generator:11.34.2")
+  api("io.ebean:ebean:12.1.3")
+  api("io.ebean:ebean-querybean:12.1.3")
+  api("io.ebean:ebean-agent:12.1.3")
+  api("io.ebean.tools:finder-generator:11.34.2")
   
-  implementation("com.vaadin:vaadin-themes:$vaadin8Version")
-  implementation("com.vaadin:vaadin-server:$vaadin8Version")
-  implementation("com.vaadin:vaadin-client-compiled:$vaadin8Version")
-  implementation("javax.servlet:javax.servlet-api:3.1.0")
+  api("com.vaadin:vaadin-themes:$vaadin8Version")
+  api("com.vaadin:vaadin-server:$vaadin8Version")
+  api("com.vaadin:vaadin-client-compiled:$vaadin8Version")
+  api("javax.servlet:javax.servlet-api:3.1.0")
   
-  implementation("mysql:mysql-connector-java:5.1.48")
+  api("mysql:mysql-connector-java:5.1.48")
   
-  implementation("org.apache.commons:commons-dbcp2:2.3.0")
+  api("org.apache.commons:commons-dbcp2:2.3.0")
   
-  implementation("org.cups4j:cups4j:0.7.6")
-  implementation("org.glassfish.jersey.core:jersey-client:2.27")
-  implementation("org.glassfish.jersey.media:jersey-media-multipart:2.27")
-  implementation("org.glassfish.jersey.inject:jersey-hk2:2.27")
-  implementation("khttp:khttp:1.0.0")
+  api("org.cups4j:cups4j:0.7.6")
+  api("org.glassfish.jersey.core:jersey-client:2.27")
+  api("org.glassfish.jersey.media:jersey-media-multipart:2.27")
+  api("org.glassfish.jersey.inject:jersey-hk2:2.27")
+  api("khttp:khttp:1.0.0")
   
-  implementation("org.xerial:sqlite-jdbc:3.21.0.1")
-  implementation("org.sql2o:sql2o:1.5.4")
+  api("org.xerial:sqlite-jdbc:3.21.0.1")
+  api("org.sql2o:sql2o:1.5.4")
   // https://mvnrepository.com/artifact/com.jolbox/bonecp
-  //implementation("com.jolbox:bonecp:0.8.0.RELEASE")
-  implementation("com.zaxxer:HikariCP:3.4.1")
+  //api("com.jolbox:bonecp:0.8.0.RELEASE")
+  api("com.zaxxer:HikariCP:3.4.1")
   
-  implementation("org.imgscalr:imgscalr-lib:4.2")
-  implementation("de.steinwedel.vaadin.addon:messagebox:4.0.21")
-  implementation("org.vaadin.patrik:GridFastNavigation:2.4.8")
-  implementation("org.vaadin:viritin:2.9")
-  //implementation("org.vaadin.crudui:crudui:2.3.1")
-  implementation("org.vaadin.addons:filtering-grid:0.1.1")
-  implementation("com.fo0.advancedtokenfield:AdvancedTokenField:0.5.1")
-  implementation("org.vaadin:grideditorcolumnfix:0.3.1")
-  implementation("com.whitestein.vaadin.widgets:wt-pdf-viewer:2.0.1")
-  //implementation("org.vaadin:grid-renderers-collection-addon:2.6.0")
+  api("org.imgscalr:imgscalr-lib:4.2")
+  api("de.steinwedel.vaadin.addon:messagebox:4.0.21")
+  api("org.vaadin.patrik:GridFastNavigation:2.4.8")
+  api("org.vaadin:viritin:2.9")
+  //api("org.vaadin.crudui:crudui:2.3.1")
+  api("org.vaadin.addons:filtering-grid:0.1.1")
+  api("com.fo0.advancedtokenfield:AdvancedTokenField:0.5.1")
+  api("org.vaadin:grideditorcolumnfix:0.3.1")
+  api("com.whitestein.vaadin.widgets:wt-pdf-viewer:2.0.1")
+  //api("org.vaadin:grid-renderers-collection-addon:2.6.0")
   // heroku app runner
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
   testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 }
 
+tasks.getByName<Jar>("jar") {
+  enabled = true
+}
 
+tasks.getByName<War>("war") {
+  enabled = true
+}
 
