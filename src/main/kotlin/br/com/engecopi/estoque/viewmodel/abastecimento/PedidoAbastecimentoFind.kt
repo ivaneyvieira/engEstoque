170dbd07ed1893d8784d9c62ebc9d9433340c17c
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-02-27 12:57:17 -0300

    Módulo de abstecimento

---
+++ src/main/kotlin/br/com/engecopi/estoque/viewmodel/abastecimento/PedidoAbastecimentoFind.kt
@@ -0,0 +1,39 @@
+package br.com.engecopi.estoque.viewmodel.abastecimento
+
+import br.com.engecopi.estoque.model.KeyNota
+import br.com.engecopi.estoque.model.Nota
+import br.com.engecopi.estoque.model.RegistryUserInfo.usuarioDefault
+import br.com.engecopi.estoque.model.TipoNota.PEDIDO_R
+import br.com.engecopi.estoque.model.TipoNota.VENDAF
+import br.com.engecopi.estoque.viewmodel.EChaveNaoEncontrada
+import br.com.engecopi.estoque.viewmodel.ENotaEntregaFutura
+import br.com.engecopi.estoque.viewmodel.ENovaFaturaLancada
+import br.com.engecopi.estoque.viewmodel.EPedidoRessuprimento
+import br.com.engecopi.saci.beans.NotaProdutoSaci
+
+class PedidoAbastecimentoFind() {
+  fun findNotaSaidaKey(key: String): List<NotaProdutoSaci> {
+    val notaSaci = when(key.length) {
+      44   -> Nota.findNotaSaidaKey(key)
+      else -> {
+        val keyNota = KeyNota(key)
+        Nota.findNotaSaidaSaci(keyNota.storeno, keyNota.numero)
+      }
+    }
+    val nota = notaSaci.firstOrNull() ?: throw EChaveNaoEncontrada()
+    val numero = nota.numero ?: ""
+    return when {
+      nota.isNotaFaturaLancada()  -> throw ENovaFaturaLancada()
+      nota.tipoNota() == VENDAF   -> throw ENotaEntregaFutura(numero)
+      nota.tipoNota() == PEDIDO_R -> throw EPedidoRessuprimento(numero)
+      else                        -> notaSaci.filtroTipoCompativel()
+    }
+  }
+  
+  private fun List<NotaProdutoSaci>.filtroTipoCompativel(): List<NotaProdutoSaci> {
+    return this.filter {nota ->
+      val tipo = nota.tipoNota() ?: return@filter false
+      return@filter usuarioDefault.isTipoCompativel(tipo)
+    }
+  }
+}
---
+++ src/main/kotlin/br/com/engecopi/estoque/viewmodel/abastecimento/PedidoAbastecimentoPrint.kt
@@ -0,0 +1,67 @@
+package br.com.engecopi.estoque.viewmodel.abastecimento
+
+import br.com.engecopi.estoque.model.Etiqueta
+import br.com.engecopi.estoque.model.ItemNota
+import br.com.engecopi.estoque.model.Nota
+import br.com.engecopi.estoque.model.StatusNota
+import br.com.engecopi.estoque.model.StatusNota.CONFERIDA
+import br.com.engecopi.estoque.model.StatusNota.INCLUIDA
+import br.com.engecopi.estoque.model.envelopes.Printer
+import br.com.engecopi.estoque.model.query.QItemNota
+
+class PedidoAbastecimentoPrint() {
+  private fun imprimir(itemNota: ItemNota?, etiqueta: Etiqueta): String {
+    itemNota ?: return ""
+    val print = itemNota.printEtiqueta()
+    itemNota.let {
+      it.refresh()
+      it.impresso = true
+      it.update()
+    }
+    return print.print(etiqueta.template)
+  }
+  
+  fun imprimir(nota: Nota?): List<PacoteImpressao> {
+    return if(nota == null) emptyList()
+    else {
+      val id = nota.id
+      val notaRef = Nota.byId(id) ?: return emptyList()
+      val listaItens = notaRef.itensNota()
+      val itensAbreviacao = listaItens.groupBy {it.abreviacao}
+      val impressaoCD: List<PacoteImpressao> = itensAbreviacao.flatMap {entry ->
+        val abreviacao = entry.key ?: return@flatMap emptyList<PacoteImpressao>()
+        val itensNota = entry.value
+        val text = imprimeItens(CONFERIDA, itensNota)
+        val impressoraName = if(abreviacao.printer == Printer.VAZIA) Printer("Localizacao ${abreviacao.abreviacao}")
+        else abreviacao.printer
+        listOf(PacoteImpressao(impressoraName, text))
+      }
+      val text = imprimeItens(INCLUIDA, listaItens)
+      val impressaoEXP = listOf(PacoteImpressao(Printer("EXP4"), text))
+      
+      impressaoCD + impressaoEXP
+    }
+  }
+  
+  private fun imprimeItens(status: StatusNota, itens: List<ItemNota>): String {
+    val etiquetas = Etiqueta.findByStatus(status, "ETEXP")
+    return etiquetas.joinToString(separator = "\n") {etiqueta ->
+      itens.map {imprimir(it, etiqueta)}
+        .distinct()
+        .joinToString(separator = "\n")
+    }
+  }
+  
+  fun imprimeTudo(): String {
+    val etiquetas = Etiqueta.findByStatus(INCLUIDA, "ETEXP")
+    val itens =
+      QItemNota().impresso.eq(false)
+        .status.eq(INCLUIDA)
+        .findList()
+    return etiquetas.joinToString(separator = "\n") {etiqueta ->
+      itens.map {item -> imprimir(item, etiqueta)}
+        .distinct()
+        .joinToString(separator = "\n")
+    }
+  }
+}
